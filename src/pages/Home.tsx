b2e1cf6ed31a168692bc82937517f88e32794b70
Luwa <luwa@MacBook-Pro-de-Luwa.local>
2021-06-25 17:34:25 -0300

    create athentication with firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,14 +5,19 @@ import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
-
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth()
 
-  function navigateToNewRoom() {
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle()
+    }
+
     history.push('/rooms/new');
   }
 
@@ -26,7 +31,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button onClick={navigateToNewRoom} className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
